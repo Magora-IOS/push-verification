0bd27e186145a9c52a964d69a2a04c509661205f
Michael Kapelko <kornerr@gmail.com>
2017-07-11 18:51:54 +0700

    Create MainVC manually

--- push-verification/AppDelegate.swift
+++ push-verification/AppDelegate.swift
@@ -5,35 +5,62 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var mainVC: MainVC?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // iOS 8-10
-        // Request permission to receive push notifications.
-        let pushSettings = UIUserNotificationSettings(types: [.alert, .sound], categories: nil)
-        application.registerUserNotificationSettings(pushSettings)
-        // Register to receive push notifications.
-        application.registerForRemoteNotifications()
-        // TODO: iOS10+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        setupMainVC()
+        setupNotifications()
         
-        
-        
+        window!.backgroundColor = UIColor.white
+        window!.makeKeyAndVisible()
 
         return true
     }
 
-    func application(_ application: UIApplication, didRegister notificationSettings: UIUserNotificationSettings) {
+    func application(
+        _ application: UIApplication,
+        didRegister notificationSettings: UIUserNotificationSettings) {
+
         NSLog("AppDelegate. Notification seetings changed to '%@'", notificationSettings)
     }
     
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+
         let tokens = deviceToken.map { String(format: "%02.2hhx", $0) }
         let token = tokens.joined()
         NSLog("AppDelegate. Device token: '%@'", token)
     }
     
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    func application(
+        _ application: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: Error) {
+
         NSLog("AppDelegate. Failed to register. ERROR: '%@'", error as NSError)
+    }
+
+    func setupMainVC() {
+        mainVC = MainVC(nibName: "MainVC", bundle: nil)
+        let nvc = UINavigationController(rootViewController: mainVC!)
+        window!.rootViewController = nvc
+    }
+
+    func setupNotifications() {
+        // iOS 8-10
+        // Request permission to receive push notifications.
+        let pushSettings = UIUserNotificationSettings(types: [.alert, .sound], categories: nil)
+        UIApplication.shared.registerUserNotificationSettings(pushSettings)
+        // Register to receive push notifications.
+        UIApplication.shared.registerForRemoteNotifications()
+
+        // TODO: iOS10+
     }
 }
 
