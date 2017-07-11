1aebdb19abeed342496ac6babe24f68704c7f9c7
Michael Kapelko <kornerr@gmail.com>
2017-07-11 14:11:55 +0700

    Initial Commit

---
+++ push-verification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  push-verification
+//
+//  Created by Michael Kapelko on 11/07/17.
+//  Copyright © 2017 Magora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
