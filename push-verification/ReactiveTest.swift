6ea6793fe0b77f26b3469c4494c2e4ebc785c4eb
Michael Kapelko <kornerr@gmail.com>
2017-07-13 12:15:56 +0700

    ReactiveSwift sample

--- push-verification/ReactiveTest.swift
+++ push-verification/ReactiveTest.swift
@@ -1,11 +1,26 @@
 
+import ReactiveSwift
+import Result
+
 import UIKit
 
 class ReactiveTest {
     
     init() {
         print("ReactiveTest.init")
-        
+
+        let sp =
+            SignalProducer<String, NoError> { (sink: Observer, disposable: Disposable) in
+                sink.send(value: "Hello, world")
+                sink.sendCompleted()
+            }
+            .map { (s: String) in
+                return s.uppercased()
+            }
+
+        sp.startWithValues { (input: String) in
+            print(input)
+        }
     }
 }
 
